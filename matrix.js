9993fec28ff79396c10ad676a57d622a29a170d2
janettec <janette6@stanford.edu>
2016-05-29 17:11:19 -0700

    a bit more css stuff

--- matrix.js
+++ matrix.js
@@ -40,33 +40,12 @@ function matrix(json) {
       .attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; })
       .each(row);
 
-  // row.append("line")
-  //     .attr("x2", width);
-
-  row.append("text")
-      .attr("x", -6)
-      .attr("y", y.rangeBand() / 2)
-      .attr("dy", ".32em")
-      .attr("text-anchor", "end")
-      .text(function(d) { return d.type_of_auth; });
-
-  // // var column = svg.selectAll(".column")
-  // //     .data(matrix)
-  // //   .enter().append("g")
-  // //     .attr("id", function(d, i) { return "col"+i; })
-  // //     .attr("class", "column")
-  // //     .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
-
-  // // column.append("line")
-  // //     .attr("x1", -width);
-
-  // // column.append("text")
-  // //     .attr("x", 6)
-  // //     .attr("y", x.rangeBand() / 2)
-  // //     .attr("dy", ".32em")
-  // //     .attr("text-anchor", "start")
-  // //     .text(function(d, i) { return nodes[i].name; });
-
+  // row.append("text")
+  //     .attr("x", -6)
+  //     .attr("y", y.rangeBand() / 2)
+  //     .attr("dy", ".32em")
+  //     .attr("text-anchor", "end")
+  //     .text(function(d) { return d.type_of_auth; });
 
   function getValArray(obj){
     valArr = [];
@@ -139,65 +118,7 @@ function matrix(json) {
             return d == 1 ? "#F2FF21" : "#FDFFE8";
           }
         });
-        // .on("mouseover", mouseover)
-        // .on("mouseout", mouseout);
   }
-
-  // function mouseover(p) {
-  //   d3.selectAll(".row text").classed("active", function(d, i) { return i == p.y; });
-  //   d3.selectAll(".column text").classed("active", function(d, i) { return i == p.x; });
-  //     d3.select(this).insert("title").text(nodes[p.y].name + "--" + nodes[p.x].name);
-  //     d3.select(this.parentElement)
-	 //  .append("rect")
-	 //  .attr("class", "highlight")
-	 //  .attr("width", width)
-	 //  .attr("height", x.rangeBand());
-  //     d3.select("#col"+p.x)
-	 //  .append("rect")
-	 //  .attr("class", "highlight")
-	 //  .attr("x", -width)
-	 //  .attr("width", width)
-	 //  .attr("height", x.rangeBand());
-  // }
-
-  // function mouseout(p) {
-  //   d3.selectAll("text").classed("active", false);
-  //   d3.select(this).select("title").remove();
-  //   d3.selectAll(".highlight").remove();
-  // }
-
-  // var currentOrder = 'name';
-
-  // function order(value) {
-  //  var o = orders[value];
-  //  currentOrder = value;
-
-  //  if (typeof o === "function") {
-  //   orders[value] = o.call();
-  //  }
-  //  x.domain(orders[value]);
-
-  //  var t = svg.transition().duration(1500);
-
-  //  t.selectAll(".row")
-  //         .delay(function(d, i) { return x(i) * 4; })
-  //         .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
-  //   .selectAll(".cell")
-  //         .delay(function(d) { return x(d.x) * 4; })
-  //         .attr("x", function(d) { return x(d.x); });
-
-  //  t.selectAll(".column")
-  //         .delay(function(d, i) { return x(i) * 4; })
-  //         .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
-  // }
-
-
-  // matrix.order = order;
-
-  // var timeout = setTimeout(function() {}, 1000);
-  // matrix.timeout = timeout;
-  
-  // return matrix;
 
   //initial order
   iorder = [];
